439450c8a4e49956398ea00d5dadd7fdc34dcc00
Angelo Gabriel <127778596+angelo-gabs@users.noreply.github.com>
2025-08-18 10:18:09 -0300

    Update homePage.js

--- cypress/pages/homePage.js
+++ cypress/pages/homePage.js
@@ -1,20 +1,85 @@
 class HomePage {
-    selectorsList(){
-        const selectors = {
-            homeTabList: ".Mui-selected",
-            newTrasactionButton: "[href='/transaction/new']",
-        }
+  selectorsList() {
+    const selectors = {
+      tabs: {
+        homeTab: ".Mui-selected",
+      },
 
-        return selectors 
-    }
-    
-    checkHomePage(){
-        cy.get(this.selectorsList().homeTabList)
-    }
-    
-    accessTransactionPage(){
-        cy.get(this.selectorsList().newTrasactionButton).click()
-    }
+      fields: {
+        bankNameField: "[name='bankName']",
+        routingNumberField: "[name='routingNumber']",
+        accountNumberField: "[name='accountNumber']",
+      },
+
+      buttons: {
+        newTransactionButton: "[href='/transaction/new']",
+        saveBankAccountInfoButton: "[data-test='bankaccount-submit']",
+        nextOnboardingButton: '[data-test="user-onboarding-next"]',
+        doneOnboardingButton: '[data-test="user-onboarding-next"]',
+      },
+
+      validations: {
+        onboardingBankInfo: "[data-test='user-onboarding-dialog-content']",
+      },
+    };
+
+    return selectors;
+  }
+
+  // ===== Page access =====
+  checkHomePage() {
+    cy.get(this.selectorsList().tabs.homeTab);
+  }
+
+  accessTransactionPage() {
+    cy.get(this.selectorsList().buttons.newTransactionButton).click();
+  }
+
+  // ===== Field interactions =====
+  focusBlurField(field) {
+    cy.get(field).focus().blur();
+  }
+
+  fillBankAccountInfo(bankName, routingNumber, accountNumber) {
+    bankName
+      ? cy.get(this.selectorsList().fields.bankNameField).type(bankName)
+      : this.focusBlurField(this.selectorsList().fields.bankNameField);
+
+    routingNumber
+      ? cy.get(this.selectorsList().fields.routingNumberField).type(routingNumber)
+      : this.focusBlurField(this.selectorsList().fields.routingNumberField);
+
+    accountNumber
+      ? cy.get(this.selectorsList().fields.accountNumberField).type(accountNumber)
+      : this.focusBlurField(this.selectorsList().fields.accountNumberField);
+  }
+
+  // ===== Buttons =====
+  clickOnboardingNextButton() {
+    cy.get(this.selectorsList().buttons.nextOnboardingButton).click({ force: true });
+  }
+
+  clickOnboardingDoneButton() {
+    cy.get(this.selectorsList().buttons.doneOnboardingButton).click({ force: true });
+  }
+
+  clickSaveBankAccountInfo() {
+    cy.get(this.selectorsList().buttons.saveBankAccountInfoButton).click();
+  }
+
+  // ===== Validations =====
+  checkOnboardingBankInfo() {
+    cy.get(this.selectorsList().validations.onboardingBankInfo).should("be.visible");
+  }
+
+  // ===== Flow =====
+  checkOnboardingExists(bankName, routingNumber, accountNumber) {
+    this.checkOnboardingBankInfo();
+    this.clickOnboardingNextButton();
+    this.fillBankAccountInfo(bankName, routingNumber, accountNumber);
+    this.clickSaveBankAccountInfo();
+    this.clickOnboardingDoneButton();
+  }
 }
 
-export default HomePage
+export default HomePage;
