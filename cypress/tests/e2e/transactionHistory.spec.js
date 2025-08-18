b33133a2612142028f7e8dae3a0ad5abf180fe34
Angelo Gabriel <127778596+angelo-gabs@users.noreply.github.com>
2025-08-18 10:14:12 -0300

    Update transactionHistory.spec.js

--- cypress/tests/e2e/transactionHistory.spec.js
+++ cypress/tests/e2e/transactionHistory.spec.js
@@ -1,23 +1,38 @@
 import userData from '../../fixtures/users/userData.json'
 import SignInPage from '../../pages/signInPage.js'
+import SignUpPage from '../../pages/signUpPage.js'
 import HomePage from '../../pages/homePage.js'
 import TransactionHistoryPage from '../../pages/transactionHistoryPage.js'
 
+
 const signInPage = new SignInPage()
+const signUpPage = new SignUpPage()
 const homePage = new HomePage()
 const transactionHistoryPage = new TransactionHistoryPage()
 
-describe('Visualizar histórico de transações com sucesso', () => {
+describe.skip('Visualizar histórico de transações com sucesso', () => {
   it('Deve exibir o histórico de transações de um usuário corretamente', () => {
     signInPage.logingIn(userData.userSignIn.userSucess.username, userData.userSignIn.userSucess.password)
     homePage.checkHomePage()
     transactionHistoryPage.accessHistoryTab()
-    transactionHistoryPage.checkHistory('be.visible')
+    transactionHistoryPage.checkHistory()
   });
 });
 
-describe.skip('Tentar visualizar o histórico de transações sem transações anteriores', () => {
+describe('Tentar visualizar o histórico de transações sem transações anteriores', () => {
   it('Deve exibir uma mensagem indicando que o usuário não possui transações anteriores', () => {
-    // Implemente os passos do caso de teste aqui
+    signUpPage.completeRegistration(
+      userData.userSignUp.firstName, 
+      userData.userSignUp.lastName, 
+      userData.userSignUp.username, // gerar um aleatório
+      userData.userSignUp.password,
+      userData.userSignUp.confirmPassword
+    )
+    signInPage.logingIn(userData.userSignUp.username, userData.userSignUp.password)
+    homePage.checkHomePage()
+    homePage.checkOnboardingExists('Tests', '123456234', '122213214') // arrumar aqui
+    transactionHistoryPage.accessHistoryTab()
+    transactionHistoryPage.checkEmptyHistory()
   });
 });
+
