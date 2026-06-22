f45080134c0498da1c822ccee2db738b1c5c9fdc
Angelo Gabriel <127778596+angelo-gabs@users.noreply.github.com>
2026-06-22 16:57:32 -0300

    Update signIn.spec.js

--- cypress/tests/e2e/signIn.spec.js
+++ cypress/tests/e2e/signIn.spec.js
@@ -3,31 +3,31 @@ import SignInPage from '../../pages/signInPage.js'
 
 const signInPage = new SignInPage()
 
-describe('Login with valid credentials', () => {
-  it('Login with valid username and password', () => {
+describe('Login - Credenciais Válidas', () => {
+  it('Login com credenciais válidas', () => {
     signInPage.logingIn(userData.userSignIn.userSucess.username, userData.userSignIn.userSucess.password)
   });
 });
 
-describe('Login with invalid credentials', () => {
+describe('Login - Credenciais inválidas', () => {
   afterEach(() => {
     signInPage.checkRequireMsg('failedLoginMessage')
   })
 
-  it('Login with invalid username and valid password', () => {
+  it('Login com nome de usuário inválido', () => {
     signInPage.logingIn(userData.userSignIn.userFail.username, userData.userSignIn.userSucess.password)
   })
 
-  it('Login with valid username and invalid password', () => {
+  it('Login com senha inválida', () => {
     signInPage.logingIn(userData.userSignIn.userSucess.username, userData.userSignIn.userFail.password)
   })
 
-  it('Login with invalid username and invalid password', () => {
+  it('Login com credenciais inválidas', () => {
     signInPage.logingIn(userData.userSignIn.userFail.username, userData.userSignIn.userFail.password)
   })
 })
 
-describe('Login with empty fields', () => {
+describe('Login - Campos vazios', () => {
   beforeEach(() => {
     signInPage.accessSignInPage()
   })
@@ -36,23 +36,23 @@ describe('Login with empty fields', () => {
     signInPage.checkSignInButton()
   })
 
-  it('Login with empty username and password fields', () => {
+  it('Login com todos os campos em branco', () => {
     signInPage.fillLoginForm('', '')
     signInPage.checkRequireMsg('usernamRequiredMsg')
     //Na página de login não é exibido nenhuma mensagem de erro quando o campo de senha é deixado em branco
   })
 
-  it('Login with empty username field', () => {
+  it('Login com campo de "username" em branco', () => {
     signInPage.fillLoginForm('', userData.userSignIn.userSucess.password)
     signInPage.checkRequireMsg('usernamRequiredMsg')
   })
 
-  it('Login with empty password field', () => {
+  it('Login com campo de "password" em branco', () => {
     signInPage.fillLoginForm(userData.userSignIn.userSucess.username, '')
     //Na página de login não é exibido nenhuma mensagem de erro quando o campo de senha é deixado em branco
   })
 
-  it('Login with password shorter than 4 characters', () => {
+  it('Login com senha contendo menos que 4 caracteres', () => {
     signInPage.fillLoginForm(userData.userSignIn.userSucess.username, userData.shortPassword)
     signInPage.focusBlurField(signInPage.selectorsList().fields.passwordField)
     signInPage.checkRequireMsg( 'passwordRequireMsg')
